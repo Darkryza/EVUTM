76f18343b68cbe8b4f80d658f5c855088e9ac767
Darkryza <82858316+Darkryza@users.noreply.github.com>
2024-11-15 16:33:34 +0800

    update

--- resources/views/include/layout.blade.php
+++ resources/views/include/layout.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="{{ asset('images/UTM-LOGO.png') }}" type="image/png">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="icon" href="{{ asset('images/UTM-LOGO.png') }}" type="image/png">
     <title>@yield('title','EVUTM')</title>
 </head>
 <body>
