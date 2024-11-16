68aee11b2870bdfabb44bbcd2e9cc96d7254bd71
Darkryza <82858316+Darkryza@users.noreply.github.com>
2024-11-17 00:18:52 +0800

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,40 @@
 @extends('include.layout')
 @section('content')
-<div class="welcome-container container">
-    <h1>Welcome to EVUTM</h1>
-    <p>This is a simple web application built using Laravel framework.</p>
+<div class="container welcome-container">
+    <div class="left">
+        <h1>Welcome to EVUTM</h1>
+        <p>The EVUTM (Events Vendor UTM) system is an innovative platform designed to promote and showcase various events hosted at UTM. It serves as a dynamic hub where the university community and the public can seamlessly engage with upcoming events. Notably, EVUTM extends an invitation to prospective vendors, offering them the opportunity to participate and contribute to the vibrant tapestry of events hosted by UTM. By facilitating this connection between organizers and vendors, EVUTM fosters a collaborative atmosphere that enriches the event experience for all involved parties.</p>
+    </div>
+    <div class="right">
+    </div>
+</div>
+<div class="container step-join-container">
+    <h1>how to join as vendor ?</h1>
+    <div class="steps">
+        <div class="step">
+            <h2>Step 1</h2>
+            <img src="{{ asset('images/gerbang-kelaur.jpg') }}" alt="">
+            <h3>scroll</h3>
+            <p>
+                Find your favourite event that you want to join.
+            </p>
+        </div>
+        <div class="step">
+            <h2>Step 1</h2>
+            <img src="{{ asset('images/gerbang-kelaur.jpg') }}" alt="">
+            <h3>scroll</h3>
+            <p>
+                Find your favourite event that you want to join.
+            </p>
+        </div>
+        <div class="step">
+            <h2>Step 1</h2>
+            <img src="{{ asset('images/gerbang-kelaur.jpg') }}" alt="">
+            <h3>scroll</h3>
+            <p>
+                Find your favourite event that you want to join.
+            </p>
+        </div>
+    </div>
 </div>
 @endsection
