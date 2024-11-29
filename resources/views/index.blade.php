ab1bb7e30a04cb2b52dcf515893091951dbf1f00
Darkryza <82858316+Darkryza@users.noreply.github.com>
2024-11-29 20:37:20 +0800

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,12 @@
         <p>The EVUTM (Events Vendor UTM) system is an innovative platform designed to promote and showcase various events hosted at UTM. It serves as a dynamic hub where the university community and the public can seamlessly engage with upcoming events. Notably, EVUTM extends an invitation to prospective vendors, offering them the opportunity to participate and contribute to the vibrant tapestry of events hosted by UTM. By facilitating this connection between organizers and vendors, EVUTM fosters a collaborative atmosphere that enriches the event experience for all involved parties.</p>
     </div>
     <div class="right">
+        <div class="welcome-images">
+            <img src="{{ asset('images/gerbang-kelaur.jpg') }}" alt="">
+            <img src="{{ asset('images/gerbang-kelaur.jpg') }}" alt="">
+            <img src="{{ asset('images/gerbang-kelaur.jpg') }}" alt="">
+            <img src="{{ asset('images/gerbang-kelaur.jpg') }}" alt="">
+        </div>
     </div>
 </div>
 <div class="container step-join-container">
@@ -30,7 +36,7 @@
         <div class="step">
             <h2>Step 3</h2>
             <img src="{{ asset('images/gerbang-kelaur.jpg') }}" alt="">
-            <h3>apply</h3>
+            <h3>tap</h3>
             <p>
                 Click the button to apply as a vendor. Then you need to fill in the form provided.
             </p>
