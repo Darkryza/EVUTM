e3852a2fb3a0c2bd30564a7358835af1d0dcbd25
Darkryza <82858316+Darkryza@users.noreply.github.com>
2024-12-05 16:49:58 +0800

    update

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,5 +9,16 @@
     </div>
     <div class="logo">
         <img src="{{ asset('images/UTM-LOGO-FULL.png') }}" alt="">
-    </div></div>
+    </div>
+    <h1>E-VENDOR SYSTEM</h1>
+    <form action="">
+        @csrf
+        <input type="text" name="fullname" id="fullname" placeholder="fullname">
+        <input type="text" name="username" id="username" placeholder="username">
+        <input type="text" name="username" id="username" placeholder="username">
+        <input type="email" name="email" id="email" placeholder="email">
+        <input type="password" name="password" id="password" placeholder="password">
+        <button>Sign Up</button>
+    </form>
+</div>
 @endsection
